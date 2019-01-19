42c017ca51c6051f4795e2b902963150f41ffafa
steve <steve.hummingbird@yandex.com>
2019-01-19 16:41:00 +0100

    Added ember-cli-github-pages addon

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -46,6 +46,8 @@ module.exports = function(environment) {
   if (environment === 'production') {
     ENV.locationType = 'hash';
     ENV.rootURL = '/ember-file-drop-zone/';
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/ember-file-drop-zone/';
     // here you can enable a production-specific feature
   }
 
